e9901d57b57e08a654aef19aca65e92c329ccec7
Damgi Ahn <damgi.dev@gmail.com>
2025-03-11 11:28:14 +0900

    [DG] work 02

--- works/work_1/main.py
+++ works/work_1/main.py
@@ -7,38 +7,43 @@ from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn import model_selection
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
 DATASET_PATH = "dataset/iris.csv"  # 본인이 iris.csv를 저장한 경로를 입력합니다.
 
 if __name__ == "__main__":
     iris_dataset = pd.read_csv(DATASET_PATH)
-    iris_dataset['target'] = iris_dataset['price_range']
+    # iris_dataset['target'] = iris_dataset['Name'].map({"Iris-setosa": 0, "Iris-versicolor": 1, "Iris-virginica": 2})
 
-    x_data = iris_dataset.iloc[:, :18]
-    y_data = iris_dataset.iloc[:, [-1]]
+    x_data = iris_dataset.iloc[:, :-1]
+    y_data = iris_dataset.iloc[:, -1] 
 
-    classifier_list = []
-    classifier_list.append(("DT", DecisionTreeClassifier()))
-    classifier_list.append(("RF", RandomForestClassifier()))
-    classifier_list.append(("SVM", SVC()))
-    classifier_list.append(("LR", LogisticRegression()))
+    X_train, X_test, Y_train, Y_test = train_test_split(x_data, y_data, test_size=0.33, random_state=42)
     
-    results = []
-    names = []
+    print("X_train: ", X_train)
+    print("X_test: ", X_test)
+    print("Y_train: ", Y_train)
+    print("Y_test: ", Y_test)
     
-    for name, classifier in classifier_list:
-        kfold = model_selection.StratifiedKFold(n_splits=5, random_state=7, shuffle=True)
-        cv_results = model_selection.cross_val_score(classifier, x_data, y_data.values.ravel(), cv=kfold, scoring="accuracy")
+    DT = DecisionTreeClassifier()
+    DT.fit(X_train, Y_train)
+    dt_pred = DT.predict(X_test)
+    print("DecisionTreeClassifier: ", accuracy_score(Y_test, dt_pred))
+    
+    rf = RandomForestClassifier()
+    rf.fit(X_train, Y_train)
+    rf_pred = rf.predict(X_test)
+    print("RandomForestClassifier: ", accuracy_score(Y_test, rf_pred))
+    
+    svm = SVC()
+    svm.fit(X_train, Y_train)
+    svm_pred = svm.predict(X_test)
+    print("SVM: ", accuracy_score(Y_test, svm_pred))
 
-        print(name, ":", cv_results)
-        results.append(cv_results)
-        names.append(name)
-            
-    fig = plt.figure()
+    lr = LogisticRegression(max_iter=3000)
+    lr.fit(X_train, Y_train)
+    lr_pred = lr.predict(X_test)
+    print("LogisticRegression: ", accuracy_score(Y_test, lr_pred))
 
-    fig.suptitle('Classifier Comparison')
-    ax = fig.add_subplot(111)
-    plt.boxplot(results)
-    ax.set_xticklabels(names)
-    plt.show()
